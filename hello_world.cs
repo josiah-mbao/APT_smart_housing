34908994b6bf6314fd8a099d861c934bdd47976a
josiah <josiahmbaomc@gmail.com>
2023-07-06 14:23:29 +0300

    wrote console log function

--- hello_world.cs
+++ hello_world.cs
@@ -1,12 +1,3 @@
-using System;
-
-namespace HelloWorld
-{
-  class Program
-  {
-    static void Main(string[] args)
-    {
-      Console.WriteLine("Hello World!");    
-    }
-  }
-}
+Console.WriteLine("Hello World!");
+Console.WriteLine("I am Learning C#");
+Console.WriteLine("It is awesome!")
