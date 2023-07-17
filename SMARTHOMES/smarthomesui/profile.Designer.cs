40fdec0d70136a1488472d0560a5c3bbe6749d1f
tkkagotho <132766990+tkkagotho@users.noreply.github.com>
2023-07-17 12:13:22 +0300

    Add files via upload
    Updates
    
    - The homepage is now displaying
    - Added more rooms
    - Updated buttons on sidebar
    - Blank profile page

---
+++ SMARTHOMES/smarthomesui/profile.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace smarthomesui
+{
+    partial class profile
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(profile));
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.iconButton1 = new FontAwesome.Sharp.IconButton();
+            this.bellButton = new FontAwesome.Sharp.IconButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.exitButton = new FontAwesome.Sharp.IconButton();
+            this.profileButton = new FontAwesome.Sharp.IconButton();
+            this.homeButton = new FontAwesome.Sharp.IconButton();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel2.SuspendLayout();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.iconButton1);
+            this.panel2.Controls.Add(this.bellButton);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(112, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1092, 107);
+            this.panel2.TabIndex = 3;
+            // 
+            // iconButton1
+            // 
+            this.iconButton1.IconChar = FontAwesome.Sharp.IconChar.Bell;
+            this.iconButton1.IconColor = System.Drawing.Color.Black;
+            this.iconButton1.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.iconButton1.Location = new System.Drawing.Point(983, 29);
+            this.iconButton1.Name = "iconButton1";
+            this.iconButton1.Size = new System.Drawing.Size(75, 70);
+            this.iconButton1.TabIndex = 8;
+            this.iconButton1.UseVisualStyleBackColor = true;
+            // 
+            // bellButton
+            // 
+            this.bellButton.IconChar = FontAwesome.Sharp.IconChar.Bell;
+            this.bellButton.IconColor = System.Drawing.Color.Black;
+            this.bellButton.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.bellButton.Location = new System.Drawing.Point(1137, 13);
+            this.bellButton.Name = "bellButton";
+            this.bellButton.Size = new System.Drawing.Size(75, 70);
+            this.bellButton.TabIndex = 7;
+            this.bellButton.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(57, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(173, 54);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Profile";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.exitButton);
+            this.panel1.Controls.Add(this.profileButton);
+            this.panel1.Controls.Add(this.homeButton);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(112, 709);
+            this.panel1.TabIndex = 2;
+            // 
+            // exitButton
+            // 
+            this.exitButton.IconChar = FontAwesome.Sharp.IconChar.DoorClosed;
+            this.exitButton.IconColor = System.Drawing.Color.Black;
+            this.exitButton.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.exitButton.Location = new System.Drawing.Point(22, 343);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(75, 70);
+            this.exitButton.TabIndex = 6;
+            this.exitButton.UseVisualStyleBackColor = true;
+            // 
+            // profileButton
+            // 
+            this.profileButton.IconChar = FontAwesome.Sharp.IconChar.UserAlt;
+            this.profileButton.IconColor = System.Drawing.Color.Black;
+            this.profileButton.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.profileButton.Location = new System.Drawing.Point(22, 229);
+            this.profileButton.Name = "profileButton";
+            this.profileButton.Size = new System.Drawing.Size(75, 70);
+            this.profileButton.TabIndex = 5;
+            this.profileButton.UseVisualStyleBackColor = true;
+            // 
+            // homeButton
+            // 
+            this.homeButton.IconChar = FontAwesome.Sharp.IconChar.House;
+            this.homeButton.IconColor = System.Drawing.Color.Black;
+            this.homeButton.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.homeButton.Location = new System.Drawing.Point(22, 127);
+            this.homeButton.Name = "homeButton";
+            this.homeButton.Size = new System.Drawing.Size(75, 70);
+            this.homeButton.TabIndex = 3;
+            this.homeButton.UseVisualStyleBackColor = true;
+            this.homeButton.Click += new System.EventHandler(this.homeButton_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(112, 104);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // profile
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1204, 709);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "profile";
+            this.Text = "profile";
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel2;
+        private FontAwesome.Sharp.IconButton bellButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private FontAwesome.Sharp.IconButton exitButton;
+        private FontAwesome.Sharp.IconButton profileButton;
+        private FontAwesome.Sharp.IconButton homeButton;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private FontAwesome.Sharp.IconButton iconButton1;
+    }
+}
---
+++ SMARTHOMES/smarthomesui/room2.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace smarthomesui
+{
+    public partial class room2 : Form
+    {
+        public room2()
+        {
+            InitializeComponent();
+        }
+
+        private void homeButton_Click(object sender, EventArgs e)
+        {
+            new homepage().Show();
+            this.Hide();
+        }
+
+        private void profileButton_Click(object sender, EventArgs e)
+        {
+            new profile().Show();
+            this.Hide();
+        }
+    }
+}
